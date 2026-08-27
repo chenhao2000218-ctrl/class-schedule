9451aeb44d9ffef1546b670975f5b9d1059c6b1d
chenhao2000218-ctrl <chenhao2000218-ctrl@users.noreply.github.com>
2026-08-28 02:48:33 +0800

    fix: 完全重新生成iOS工程，修复中文乱码

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,15 +4,15 @@ import UserNotifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-    override func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        // 请求通知权限
-        let center = UNUserNotificationCenter.current()
-        center.requestAuthorization(options: [.alert, .badge, .sound]) { _, _ in }
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    // 请求通知权限
+    let center = UNUserNotificationCenter.current()
+    center.requestAuthorization(options: [.alert, .badge, .sound]) { _, _ in }
 
-        GeneratedPluginRegistrant.register(with: self)
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
